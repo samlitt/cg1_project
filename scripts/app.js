1caac64fe8cc546b2922c8eb76714f51d4483e1e
Vincent Klingemann <56515214+samlitt@users.noreply.github.com>
2023-07-05 19:41:06 +0200

    setting up scene

--- scripts/app.js
+++ scripts/app.js
@@ -35,7 +35,6 @@ const textureShadingProgram = await createProgram(gl, "./shader/texture_shading"
 
 // Objects
 
-const faucet = await createObject(gl, sphereMappingProgram, './assets/faucet.obj')
 const lime = await createObject(gl, textureShadingProgram, "./assets/lime.obj");
 
 const ipad = await createObjectWithMaterials(gl, textureShadingProgram, './assets/ipad.obj', './assets/ipad.mtl')
@@ -44,6 +43,23 @@ ipad.material.diffuse = [0.8, 0.8, 0.8]
 
 const ipadScreen = await createObject(gl, videoProgram, './assets/ipad_screen.obj')
 const skybox = await createSkyboxSphere(gl, skyboxProgram, './assets/skybox.obj', '/assets/skybox.jpg')
+
+const counter_base = await createObjectWithMaterials(gl, textureShadingProgram, './assets/counter_base.obj', './assets/counter_base.mtl');
+counter_base.material.ambient = [0.7, 0.3, 0.2];
+
+const counter_top = await createObjectWithMaterials(gl, textureShadingProgram, './assets/counter_top.obj', './assets/counter_top.mtl');
+counter_top.material.ambient = [0.5, 0.5, 0.5];
+
+const faucet = await createObject(gl, sphereMappingProgram, './assets/faucet.obj');
+
+const sink = await createObjectWithMaterials(gl, basicShadingProgram, './assets/sink.obj', './assets/sink.mtl');
+sink.material.ambient = [0.25, 0.25, 0.25];
+// sink.material.diffuse = [0.6, 0.6, 0.6];
+sink.material.specular = [0.8, 0.8, 0.8];
+
+const cutting_board = await createObjectWithMaterials(gl, textureShadingProgram, './assets/cutting_board.obj', './assets/cutting_board.mtl');
+cutting_board.material.ambient = [0.5, 0.5, 0.5]
+
 skybox.texture.load(sphereMappingProgram, 'u_skybox')
 
 // Lights
@@ -73,6 +89,9 @@ lightGroup.apply(textureShadingProgram)
 let lime_texture = createTexture(gl, await loadImage("assets/lime_albedo.jpg"), 1, true, true);
 const ipad_texture = createTexture(gl, await loadImage('assets/ipad.jpg'), 2, true, true)
 const video_texture = createTexture(gl, await loadVideo('./assets/video.mp4'), 3, false, false);
+const counter_base_texture = createTexture(gl, await loadImage("./assets/wood_counter_base.jpg"), 4, true, true);
+const counter_top_texture = createTexture(gl, await loadImage("./assets/marble_counter_top.jpg"), 5, true, true);
+const cutting_board_texture = createTexture(gl, await loadImage("./assets/cutting_board.jpg"), 6, true, true);
 
 // Materials
 
@@ -102,7 +121,7 @@ lime.material = limeMaterial;
 
 const camera = createCamera(gl)
 
-camera.configure([0.0, 2.0, -5.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0], toRadian(45), canvas.width / canvas.height)
+camera.configure([2, 2.0, 1.2], [-1, 1, -1], [0.0, 1.0, 0.0], toRadian(45), canvas.width / canvas.height)
 camera.apply(basicShadingProgram)
 camera.apply(sphereMappingProgram)
 camera.apply(skyboxProgram)
@@ -151,8 +170,8 @@ mat4.translate(ipadWorldMatrix, ipadWorldMatrix, [0, 0, 0])
 mat4.scale(ipadWorldMatrix, ipadWorldMatrix, [0.1, 0.1, 0.1])
 mat4.rotate(ipadWorldMatrix, ipadWorldMatrix, toRadian(90), [0, 0, 1])
 mat4.rotate(ipadWorldMatrix, ipadWorldMatrix, toRadian(-90), [1, 0, 0])
-ipad.worldMatrix = ipadWorldMatrix
-ipadScreen.worldMatrix = ipadWorldMatrix
+// ipad.worldMatrix = ipadWorldMatrix
+// ipadScreen.worldMatrix = ipadWorldMatrix
 
 const teapot1 = await createObject(gl, basicShadingProgram, './assets/teapot.obj')
 teapot1.material = basicMaterial
@@ -174,6 +193,20 @@ teapotCenter.material = basicMaterial
 mat4.translate(teapotCenter.worldMatrix, teapotCenter.worldMatrix, [-1, 0, 3])
 mat4.scale(teapotCenter.worldMatrix, teapotCenter.worldMatrix, [0.3, 0.3, 0.3])
 
+const counterWorldMatrix = new Float32Array(16);
+mat4.identity(counterWorldMatrix);
+mat4.translate(counterWorldMatrix, counterWorldMatrix, [-3, -1, 0]);
+mat4.scale(counterWorldMatrix, counterWorldMatrix, [1, 1, 1]);
+// mat4.rotate(counterWorldMatrix, counterWorldMatrix, toRadian(-45), [0, 1, 0]);
+counter_base.worldMatrix = counterWorldMatrix;
+counter_top.worldMatrix = counterWorldMatrix;
+
+faucet.worldMatrix = counterWorldMatrix;
+sink.worldMatrix = counterWorldMatrix;
+cutting_board.worldMatrix = counterWorldMatrix;
+lime.worldMatrix = counterWorldMatrix;
+ipad.worldMatrix = counterWorldMatrix;
+ipadScreen.worldMatrix = counterWorldMatrix;
 
 // Render Loop
 
@@ -204,9 +237,6 @@ function render() {
 
 	skybox.draw()
 
-	// -- Faucet
-
-	// faucet.draw(camera)
 
 	// -- Lime
 
@@ -217,17 +247,33 @@ function render() {
 
 	ipad_texture.load(textureShadingProgram, 'u_sampler')
 	ipad.draw(camera)
-
 	video_texture.update()
 	video_texture.load(videoProgram, 'u_texture')
 	ipadScreen.draw(camera)
 
-	// -- Test Teapots
 
-	teapot1.draw(camera)
-	teapot2.draw(camera)
-	teapot3.draw(camera)
-	teapotCenter.draw(camera)
+	// -- Counter
+
+	counter_base_texture.load(textureShadingProgram, 'u_sampler');
+	counter_base.draw(camera)
+	counter_top_texture.load(textureShadingProgram, 'u_sampler');
+	counter_top.draw(camera);
+
+	// -- Faucet and Sink
+
+	faucet.draw(camera)
+	sink.draw(camera);
+
+	// -- Cutting Board
+
+	cutting_board_texture.load(textureShadingProgram, 'u_sampler');
+	cutting_board.draw(camera)
+
+	// -- Knife
+
+
+
+	// -- Test Teapots
 
 	requestAnimationFrame(render);
 }
