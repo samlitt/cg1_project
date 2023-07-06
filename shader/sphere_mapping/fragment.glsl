d2840a2d7d2ad708c0b2387e7861601c31081b18
Vincent Köckerling <v.koeckerling@icloud.com>
2023-07-06 17:39:49 +0200

    Rotate sphere map

--- shader/sphere_mapping/fragment.glsl
+++ shader/sphere_mapping/fragment.glsl
@@ -10,8 +10,10 @@ uniform sampler2D u_skybox;
 float pi = 3.1415926;
 
 void main() {
+	vec3 camDir = -v_camDir; // "Rotate" Sphere Map 180°
+
 	vec3 normal = normalize(v_normal);
-	vec3 texCoordsDir = reflect(-v_camDir, normal);
+	vec3 texCoordsDir = reflect(-camDir, normal);
 
 	vec2 texCoords = vec2(0.0);
 	texCoords.s = -atan(texCoordsDir.z, texCoordsDir.x) / (2.0*pi) + 0.5;
